5de97011590f815693b2b4dc8b1ab41f6abcd97e
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-03 15:29:43 +0100

    start repo

---
+++ js/script.js
@@ -0,0 +1,108 @@
+// Esercizio: seguire le 3 milestone dell'esercizio delle Icone
+//  visto insieme stamattina.
+//  (la traccia è nelle slide della seconda parte di ES6)
+// Consigli:
+// prima di passare alla milestone 2, completate ogni aspetto della 1;
+// preparate un html/css base per gestire le milestone e miglioratelo successivamente,
+//  solo se avete completato le 3 milestone;
+// è meglio chiudere una sola milestone bene,
+// piuttosto che farle tutte e 3 ma lasciandosi alle spalle delle cose non corrette.
+
+const icons = [
+      {
+        name: "dog",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "fish",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "tractor",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "hippo",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "spider",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "apple-alt",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "carrot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "lemon",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "dragon",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "pepper-hot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "space-shuttle",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "truck-monster",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "wheelchair",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "tram",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "fighter-jet",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "helicopter",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+    ];
